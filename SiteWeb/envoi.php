a4a4ebf8a1c5c9ccef89bd64401af4d49a4d76ef
Solenn KEROULLAS <keroullasolenn@gmail.com>
2018-05-07 09:09:14 +0200

    Update Code WebSite

--- SiteWeb/envoi.php
+++ SiteWeb/envoi.php
@@ -1,4 +1,13 @@
 <?php
-$to : "keroullasolenn@gmail.com";
-echo 'caca';
+$to = "keroullasolenn@gmail.com";
+$name = $_POST['name'];
+$from = $_POST['mail'];
+$subject = $_POST['subject'];
+$message = "Ce message a été envoyé par" . $from . " : " . $_POST['message'];
+
+if(mail($to,$subject,$message,$name)) {
+    echo '<script type="text/javascript">window.alert("Votre mail a bien été envoyé ! Vous allez être redirigé"); window.location.href="/"; </script>';
+} else {
+     echo '<script type="text/javascript">window.alert("Votre mail n\'a pas été envoyé; Veuillez recommencer s\'il vous plaît"); window.location.href="contact.html"; </script>';
+}
 ?>
